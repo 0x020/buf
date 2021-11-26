0e38a26753f0d29327eeb27ccafa0a28c7e626c0
endormi <39559256+endormi@users.noreply.github.com>
2021-11-27 01:00:02 +0200

    Add buffer overflow code

---
+++ buf.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void buffer(char* i) {
+  char buf[8];
+  strcpy(buf, i);
+  printf("Input: %s\n", buf);
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    printf("Usage ./buf <input>\n");
+    exit(EXIT_FAILURE);
+  }
+
+  buffer(argv[1]);
+
+  printf("Not buffer overflow.\n");
+
+  return 0;
+}
