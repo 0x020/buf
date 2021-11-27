a3cf9d21c6c1c71df6023d165f4886c60611ad88
endormi <39559256+endormi@users.noreply.github.com>
2021-11-27 22:29:34 +0200

    Declare function

--- buf.c
+++ buf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "buf.h"
 
 void buffer(char* i) {
   char buf[8];
---
+++ buf.h
@@ -0,0 +1,6 @@
+#ifndef BUF_H
+#define BUF_H
+
+void buffer(char* i);
+
+#endif
